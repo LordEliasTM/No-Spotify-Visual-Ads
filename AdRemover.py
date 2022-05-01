41520bf22087ef0dfb38e5bcaa4c813e6f919f16
Tortuga <55048695+LordEliasTM@users.noreply.github.com>
2022-05-01 23:08:41 +0200

    Update AdRemover.py

--- AdRemover.py
+++ AdRemover.py
@@ -4,8 +4,8 @@ import requests
 import subprocess
 from websocket import create_connection
 
-#Config (EDIT THIS ACCORDING TO YOUR USER NAME!!!)
-spotify_path="C:\\Users\\YourUserName\\AppData\\Roaming\\Spotify\\Spotify.exe"
+#Config
+spotify_path="C:\\Users\\Elias\\AppData\\Roaming\\Spotify\\Spotify.exe"
 
 #Defining stuff
 base_url = 'http://localhost:4693/json/list'
@@ -21,15 +21,15 @@ script = """
   """
 payload = { 'id': 1337, 'method': 'Runtime.evaluate', 'params': {'expression': script} }
 
-#Start spotify with open remote debugging port
+#Start spotify with remote debugging
 subprocess.Popen([spotify_path, '--processStart', "Spotify.exe",
                   '--process-start-args', '--remote-debugging-port=4693'], cwd=os.path.dirname(spotify_path))
 
-#Gather spotifys devtools websocket link
+#Gather the websocket link
 req = requests.get(base_url).json()
 for a in req:
   ws_url = a.get('webSocketDebuggerUrl', 'ws://localhost:4693/devtools/page/{}'.format(a['id']))
 
-#Create websocket and send the javascript payload
+#Create websocket and send the payload
 ws = create_connection(ws_url, timeout=100)
 ws.send(json.dumps(payload))
